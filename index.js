4e4d523e2f0ae8ff9a9e9de75ad9ed424890d332
jacob <jacobharv00@gmail.com>
2021-06-11 08:21:00 -0600

    done

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function addingEventListener() {
     const input = document.getElementById('input')
 
 function clickAlert() {
-  alert('I was clicked!')
+  alert('I was clicked!!')
 }
     input.addEventListener('click', clickAlert)
 }
